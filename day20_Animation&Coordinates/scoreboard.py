73ee35ef680c529c90f2bee59f68c6ac962fff80
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2022-08-25 18:45:49 -0700

    Add files via upload

---
+++ day20_Animation&Coordinates/scoreboard.py
@@ -0,0 +1,26 @@
+from turtle import Turtle
+
+ALIGNMENT = "center"
+FONT = ("Arial", 24, "normal")
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0, 270)
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.hideturtle()
+
+    def game_over(self):
+        self.color("white")
+        self.goto(0, 0)
+        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
