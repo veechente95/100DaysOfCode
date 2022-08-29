a3d4c35a797ccf67f8a6d6dda3bacb027b99ffa3
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2022-08-28 21:12:30 -0700

    Add files via upload

---
+++ day23_TurtleCrossing_Game/player.py
@@ -0,0 +1,32 @@
+from turtle import Turtle
+
+
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 15
+FINISH_LINE_Y = 280
+
+
+class Player(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.color("green")
+        self.penup()
+        self.goto(STARTING_POSITION)
+        self.setheading(90)
+
+    def move_up(self):
+        self.forward(MOVE_DISTANCE)
+
+    def move_down(self):
+        self.forward(MOVE_DISTANCE * -1)
+
+    def successful_cross(self):
+        if self.ycor() > FINISH_LINE_Y:
+            return True
+        else:
+            return False
+
+    def go_to_start(self):
+        self.goto(STARTING_POSITION)
