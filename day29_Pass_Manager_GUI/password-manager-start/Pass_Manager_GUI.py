b818a6b8d188013360045a2852fc96edf5cda2dc
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2022-09-15 11:57:25 -0700

    Update Pass_Manager_GUI.py

--- day29_Pass_Manager_GUI/password-manager-start/Pass_Manager_GUI.py
+++ day29_Pass_Manager_GUI/password-manager-start/Pass_Manager_GUI.py
@@ -1,9 +1,31 @@
 from tkinter import *
 from tkinter import messagebox
+from random import choice, randint, shuffle
+import pyperclip   # can copy text to clipboard automatically
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v',
+               'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+               'R',
+               'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-# ---------------------------- SAVE PASSWORD ------------------------------- #
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+
+    password_list = password_letters + password_symbols + password_numbers
+    shuffle(password_list)
+
+    password = "".join(password_list)  # adds character in between lists, in this case an empty space
+    password_entry.insert(0, password)
+    pyperclip.copy(password)    # auto copies generated pass to clipboard
+
+
+# ------------------------------ SAVE PASSWORD ------------------------------- #
 def save():
     website = website_entry.get()
     email = email_entry.get()
@@ -21,6 +43,7 @@ def save():
                 data_file.write(f"{website} | {email} | {password}\n")
                 website_entry.delete(0, END)  # delete from first entry to end
                 password_entry.delete(0, END)  # delete from first entry to end
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -51,10 +74,10 @@ password_entry = Entry(width=22)
 password_entry.grid(column=1, row=3)
 
 # Buttons
-generate_button = Button(text="Generate Password")
+generate_button = Button(text="Generate Password", command=generate_password)
 generate_button.grid(column=2, row=3)
 add_button = Button(text="Add", width=38, command=save)
 add_button.grid(column=1, row=4, columnspan=2)
 
-
+# Always leave at end of program
 window.mainloop()
