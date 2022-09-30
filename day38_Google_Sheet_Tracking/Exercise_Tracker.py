702ba41468f6ab89e5f2a09e027185f46248e834
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2022-09-29 22:29:54 -0700

    Update Exercise_Tracker.py

--- day38_Google_Sheet_Tracking/Exercise_Tracker.py
+++ day38_Google_Sheet_Tracking/Exercise_Tracker.py
@@ -1,22 +1,22 @@
 import requests
 from datetime import datetime
 
-GENDER = "male"
-WEIGHT_KG = 88.5
-HEIGHT_CM = 182
-AGE = 26
+GENDER = YOUR GENDER
+WEIGHT_KG = YOUR WEIGHT
+HEIGHT_CM = YOUR HEIGHT
+AGE = YOUR AGE
 
-NUTRITIONIX_APP_ID = "7079d448"
-NUTRITIONIX_API_KEY = "19d138ed3abd7db26578daa770a75c2f"
+APP_ID = YOUR NUTRITIONIX APP ID
+API_KEY = YOUR NUTRITIONIX API KEY
 
 exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
-sheety_endpoint = "https://api.sheety.co/60c07e0c70f28470eed68124215e1ba1/workoutTracking/sheet1"
+sheet_endpoint = YOUR SHEETY ENDPOINT
 
 exercise_text = input("Tell me which exercises you did: ")
 
 headers = {
-    "x-app-id": NUTRITIONIX_APP_ID,
-    "x-app-key": NUTRITIONIX_API_KEY,
+    "x-app-id": APP_ID,
+    "x-app-key": API_KEY,
 }
 
 parameters = {
@@ -27,10 +27,10 @@ parameters = {
     "age": AGE
 }
 
-response = requests.post(url=exercise_endpoint, json=parameters, headers=headers)
+response = requests.post(exercise_endpoint, json=parameters, headers=headers)
 result = response.json()
 
-# POST command to add data to sheety
+
 today_date = datetime.now().strftime("%d/%m/%Y")
 now_time = datetime.now().strftime("%X")
 
@@ -45,6 +45,6 @@ for exercise in result["exercises"]:
         }
     }
 
+    sheet_response = requests.post(sheet_endpoint, json=sheet_inputs)
 
-sheet_response = requests.post(url=sheety_endpoint, json=sheet_inputs)
-print(result)
+    print(sheet_response.text)
