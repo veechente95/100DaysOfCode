c96a517219e7e7f35888dd4fec9e1529c59028f5
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2022-11-04 14:40:44 -0700

    Update spotify_playlist.py
    Search Spotify for song titles

--- day46_Musical_Time_Machine/spotify_playlist.py
+++ day46_Musical_Time_Machine/spotify_playlist.py
@@ -27,3 +27,17 @@ sp = spotipy.Spotify(
 )
 user_id = sp.current_user()["id"]
 print(user_id)
+
+#Searching Spotify for songs by title
+song_uris = []
+year = date.split("-")[0]
+for song in song_names:
+    result = sp.search(q=f"track:{song} year:{year}", type="track")
+    print(result)
+    try:
+        uri = result["tracks"]["items"][0]["uri"]
+        song_uris.append(uri)
+    except IndexError:
+        print(f"{song} doesn't exist in Spotify. Skipped.")
+
+
