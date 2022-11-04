de1d0eec4eaf8b7675fe502d4145df55b328108e
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2022-11-04 14:40:05 -0700

    Update spotify_playlist.py
    Added nSpotify Authentication

--- day46_Musical_Time_Machine/spotify_playlist.py
+++ day46_Musical_Time_Machine/spotify_playlist.py
@@ -3,8 +3,8 @@ import requests
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
-CLIENT_ID = "6c44f40988c94ee6a7d270c9db227d40"
-CLIENT_SECRET = "2ed66530dd954b45abd23c9ba5c139cf"
+CLIENT_ID = "ADD YOUR CLIENT ID HERE"
+CLIENT_SECRET = "ADD YOUR CLIENT SECRET"
 REDIRECT_URI = "https://example.com/callback/"
 
 # Scraping Billboard 100
@@ -14,3 +14,16 @@ soup = BeautifulSoup(response.text, 'html.parser')
 song_names_spans = soup.find_all("span", class_="chart-element__information__song")
 song_names = [song.getText() for song in song_names_spans]
 
+#Spotify Authentication
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        scope="playlist-modify-private",
+        redirect_uri="http://example.com",
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        show_dialog=True,
+        cache_path="token.txt"
+    )
+)
+user_id = sp.current_user()["id"]
+print(user_id)
