3a199f0db262d95f35a404798e98688f57abbe31
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2022-11-08 21:00:45 -0800

    Update Amazon_Tracker.py

--- day47_Amazon_Price_Checker/Amazon_Tracker.py
+++ day47_Amazon_Price_Checker/Amazon_Tracker.py
@@ -25,3 +25,18 @@ print(price_as_float)
 
 title = soup.find(id="productTitle").get_text().strip()
 print(title)
+
+
+BUY_PRICE = 40
+
+if price_as_float < BUY_PRICE:
+    message = f"{title} is now {price}"
+
+    with smtplib.SMTP(YOUR_SMTP_ADDRESS, port=587) as connection:
+        connection.starttls()
+        result = connection.login(YOUR_EMAIL, YOUR_PASSWORD)
+        connection.sendmail(
+            from_addr=YOUR_EMAIL,
+            to_addrs=YOUR_EMAIL,
+            msg=f"Subject:Amazon Price Alert!\n\n{message}\n{url}"
+        )
