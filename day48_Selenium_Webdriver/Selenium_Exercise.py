dc9edb9b82f235291223149c718f919c66ae6be1
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2022-11-10 13:58:55 -0800

    Update Selenium_Exercise.py

--- day48_Selenium_Webdriver/Selenium_Exercise.py
+++ day48_Selenium_Webdriver/Selenium_Exercise.py
@@ -2,8 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 #TODO: Extract upcoming events from python.org website and put them in a nested dict
-chrome_drover_path = "/Users/veechente/Desktop/Development/chromedriver"
-driver = webdriver.Chrome(executable_path=chrome_drover_path)
+chrome_driver_path = "YOUR CHROME DRIVER PATH"
+driver = webdriver.Chrome(executable_path=chrome_driver_path)
 driver.get("https://www.python.org")
 
 event_times = driver.find_elements(By.CSS_SELECTOR, ".event-widget time")
