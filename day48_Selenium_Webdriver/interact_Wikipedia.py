5a0ff535559e30dfb5a57c012a191bf3f11b1185
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2022-11-10 13:56:57 -0800

    Add files via upload

---
+++ day48_Selenium_Webdriver/interact_Wikipedia.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+
+chrome_driver_path = "/Users/veechente/Desktop/Development/chromedriver"
+driver = webdriver.Chrome(executable_path=chrome_driver_path)
+driver.get("https://en.wikipedia.org/wiki/Main_Page")
+
+# TODO: Click on webpage
+# all_portals = driver.find_element(By.LINK_TEXT, "All portals")
+# all_portals.click()
+
+# TODO: Type on webpage
+search = driver.find_element(By.NAME, "search")
+search.send_keys("Python")
+search.send_keys(Keys.ENTER)
