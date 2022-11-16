41b3908f6d086320d7f5dec6ca2845af295dc335
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2022-11-15 16:22:40 -0800

    Update ex_main_turtle.py

--- day16_OOP/ex_main_turtle.py
+++ day16_OOP/ex_main_turtle.py
@@ -1,30 +1,12 @@
 import turtle
 from turtle import Turtle, Screen
 
-# create a new turtle object
-# OUTPUT --- <turtle.Turtle object at 0x1090a5c90>
-# new class object created saved at location in computer
+michelangelo = Turtle()
+michelangelo.shape("turtle")
+michelangelo.color("green")
+michelangelo.forward(100)
 
-
-timmy = Turtle()
-print(timmy)
-timmy.shape("turtle")
-timmy.color("green")
-
-
-def turtle_turn():
-    turtle.forward(50)
-    turtle.right(90)
-
-turtle_turn()
-turtle_turn()
-turtle_turn()
-turtle_turn()
-
+my_screen = Screen()
 
 # exit on click allows program to run until window is clicked
-my_screen = Screen()
-print(my_screen.canvheight)
 my_screen.exitonclick()
-
-
