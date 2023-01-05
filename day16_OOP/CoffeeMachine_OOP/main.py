dc652fa1eb6526afeb55fe357eaf2e3bffbe80da
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2023-01-04 17:14:12 -0800

    Update main.py

--- day16_OOP/CoffeeMachine_OOP/main.py
+++ day16_OOP/CoffeeMachine_OOP/main.py
@@ -1,12 +1,11 @@
 # Building a coffee machine again using OOP this time
-
-from menu import Menu
+from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 # create some objects from classes
-money_machine = MoneyMachine()
 coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
 menu = Menu()
 
 is_on = True
@@ -17,11 +16,9 @@ while is_on:
     if choice == "off":
         is_on = False
     elif choice == "report":
-        money_machine.report()
         coffee_maker.report()
+        money_machine.report()
     else:
         drink = menu.find_drink(choice)
         if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
             coffee_maker.make_coffee(drink)
-
-
