be1c8a6524ee9daf9b154b5686980e2e9a75f7e5
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2023-01-11 18:53:06 -0800

    Update RockPaperScissors.py

--- day4_RandomizationAndLists/RockPaperScissors.py
+++ day4_RandomizationAndLists/RockPaperScissors.py
@@ -8,7 +8,6 @@ rock = '''
       (____)
 ---.__(___)
 '''
-
 paper = '''
     _______
 ---'   ____)____
@@ -17,7 +16,6 @@ paper = '''
          _______)
 ---.__________)
 '''
-
 scissors = '''
     _______
 ---'   ____)____
@@ -27,24 +25,33 @@ scissors = '''
 ---.__(___)
 '''
 
-game_images = [rock, paper, scissors]
+game_on = True
 
-user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors: "))
-print(game_images[user_choice])
+while game_on:
+    game_images = [rock, paper, scissors]
+    user_choice = int(input("Please select '0' for rock, '1' for paper or '2' for scissors: "))
+    computer_choice = random.randint(0, 2)
 
-computer_choice = random.randint(0, 2)
-print("Computer chose:")
-print(game_images[computer_choice])
+    if user_choice >= 3 or user_choice < 0:
+        print("Invalid choice. Please select 0, 1, or 2.")
+    elif user_choice == 0 and computer_choice == 2:
+        print("YOU WIN! Rock beats scissors.")
+        print(game_images[user_choice])
+        print(game_images[computer_choice])
+    elif user_choice == 1 and computer_choice == 0:
+        print("YOU WIN! Paper beats rock.")
+        print(game_images[user_choice])
+        print(game_images[computer_choice])
+    elif user_choice == 2 and computer_choice == 1:
+        print("YOU WIN! Scissors beats paper.")
+        print(game_images[user_choice])
+        print(game_images[computer_choice])
+    else:
+        print("Computer Wins :(")
+        print(game_images[user_choice])
+        print(game_images[computer_choice])
 
-if user_choice == 0 and computer_choice == 2:
-  print("You win!")
-elif user_choice > computer_choice:
-  print("You win!")
-elif computer_choice == 0 and user_choice == 2:
-  print("You lose")
-elif computer_choice > user_choice:
-  print("You lose")
-elif user_choice >= 3 or user_choice < 0:
-  print("You typed an invalid number, you lose!")
-elif computer_choice == user_choice:
-  print("It's a draw")
+    play_again = (input("Would you like to play again? Please select 'yes' or 'no': ")).lower()
+    if play_again == "no":
+        print("Bye! Thanks for playing!")
+        game_on = False
