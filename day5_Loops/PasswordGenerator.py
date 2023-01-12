99d81462952c68af1c8ac1c21a05d4179ec35189
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2023-01-12 15:27:43 -0800

    Update PasswordGenerator.py

--- day5_Loops/PasswordGenerator.py
+++ day5_Loops/PasswordGenerator.py
@@ -1,28 +1,35 @@
 import random
 
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
-number_letters = int(input("How many letters would you like in your password?\n"))
-number_symbols = int(input(f"How many symbols would you like?\n"))
-number_numbers = int(input(f"How many numbers would you like?\n"))
+num_letters = int(input("How many letters would you like in your password?\n"))
+num_numbers = int(input(f"How many numbers would you like?\n"))
+num_symbols = int(input(f"How many symbols would you like?\n"))
 
-#Hard Level - Order of characters randomised:
+#Hard Level - Order of characters randomised using random.shuffle() method
+#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
 password_list = []
-for characters in range(1, number_letters + 1):     # letters +1 captures end of range to not exclude value
-    password_list.append(random.choice(letters))
-for characters in range(1, number_symbols + 1):
-    password_list += random.choice(symbols)
-for characters in range(1, number_numbers + 1):
+for character in range(1, num_letters + 1):   # +1 captures end value range to not get cut off
+    password_list += random.choice(letters)
+
+for character in range(1, num_numbers + 1):
     password_list += random.choice(numbers)
 
+for character in range(1, num_symbols + 1):
+    password_list += random.choice(symbols)
+
+# prints password in list
 random.shuffle(password_list)
 print(password_list)
 
+# prints password as string
 password = ""
-for char in password_list:
-    password += char
+for character in password_list:
+    password += character
 
-print(f"Your password is: {password}")
+print(f"Your password is {password}")
+
+
