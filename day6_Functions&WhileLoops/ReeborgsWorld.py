211400730c45523e5b7db50d93b447a97914381f
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2023-01-16 18:02:55 -0800

    Update ReeborgsWorld.py

--- day6_Functions&WhileLoops/ReeborgsWorld.py
+++ day6_Functions&WhileLoops/ReeborgsWorld.py
@@ -1,20 +1,40 @@
 # Go to this website and select Maze
 # https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
 
+def turn_around():
+    turn_left()
+    turn_left()
+
 def turn_right():
     turn_left()
     turn_left()
     turn_left()
 
-while front_is_clear():
+def make_square():
+    turn_left()
     move()
-turn_left()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
 
-while not at_goal():
-    if right_is_clear():
-        turn_right()
+def maze():
+    turn_left()
+    while wall_on_right():
         move()
-    elif front_is_clear():
+    turn_right()
+    move()
+    turn_right()
+    while front_is_clear():
         move()
-    else:
-        turn_left()
+    turn_left()
+
+    while not at_goal():
+        if wall_in_front():
+            jump()
+        else:
+            move()
+
+maze()
