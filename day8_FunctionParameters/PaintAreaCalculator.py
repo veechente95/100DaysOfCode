edf0f48a7ff0e3c4413ff985703b82b957e46724
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2023-01-17 13:38:33 -0800

    Update PaintAreaCalculator.py

--- day8_FunctionParameters/PaintAreaCalculator.py
+++ day8_FunctionParameters/PaintAreaCalculator.py
@@ -5,9 +5,13 @@ test_h = int(input("Height of wall: "))
 test_w = int(input("Width of wall: "))
 coverage = 5
 
+
 def paint_calc(height, width, cover):
-    num_of_cans = math.ceil((test_h * test_w) / cover)
+    area = height * width
+    # math.ceil "ceiling" rounds to whole number
+    num_of_cans = math.ceil(area / cover)   
     print(f"You will need {num_of_cans} cans of paint")
+
 
 paint_calc(height=test_h, width=test_w, cover=coverage)
 
