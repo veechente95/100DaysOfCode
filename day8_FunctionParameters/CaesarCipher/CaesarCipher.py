c942a2170dd81325b81405f3110fe291a612e755
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2023-01-17 22:04:50 -0800

    Update CaesarCipher.py

--- day8_FunctionParameters/CaesarCipher/CaesarCipher.py
+++ day8_FunctionParameters/CaesarCipher/CaesarCipher.py
@@ -14,26 +14,42 @@ text = input("Type your message: ").lower()
 shift = int(input("Type the shift number: "))
 
 
-def encrypt(plain_text, shift_amount):
-    cipher_text = ""
-    for letter in plain_text:
+# TODO-7: Combine encrypt() and decrypt() functions into a single function called caesar()
+def caesar(start_text, shift_amount, cipher_direction):
+    end_text = ""
+    if cipher_direction == "decode":
+        shift_amount *= -1
+    for letter in start_text:
         position = alphabet.index(letter)
         new_position = position + shift_amount
-        new_letter = alphabet[new_position]
-        cipher_text += new_letter
-    print(f"The encoded text is {cipher_text}")
+        end_text += alphabet[new_position]
+    print(f"The {cipher_direction} text is {end_text}")
 
 
-def decrypt(cipher_text, shift_amount):
-    plain_text = ""
-    for letter in cipher_text:
-        position = alphabet.index(letter)
-        new_position = position - shift_amount
-        plain_text += alphabet[new_position]
-    print(f"The decoded text is {plain_text}")
+# TODO-8: Call the caesar() function w/ 'text', 'shift' and 'direction' values
+caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
 
 
-if direction == "encode":
-    encrypt(plain_text=text, shift_amount=shift)
-elif direction == "decode":
-    decrypt(cipher_text=text, shift_amount=shift)
+# TODO: The code below has been condensed into a single function called caesar()
+# def encrypt(plain_text, shift_amount):
+#     cipher_text = ""
+#     for letter in plain_text:
+#         position = alphabet.index(letter)
+#         new_position = position + shift_amount
+#         cipher_text += new_letter
+#     print(f"The encoded text is {cipher_text}")
+#
+#
+# def decrypt(cipher_text, shift_amount):
+#     plain_text = ""
+#     for letter in cipher_text:
+#         position = alphabet.index(letter)
+#         new_position = position - shift_amount
+#         plain_text += alphabet[new_position]
+#     print(f"The decoded text is {plain_text}")
+#
+#
+# if direction == "encode":
+#     encrypt(plain_text=text, shift_amount=shift)
+# elif direction == "decode":
+#     decrypt(cipher_text=text, shift_amount=shift)
