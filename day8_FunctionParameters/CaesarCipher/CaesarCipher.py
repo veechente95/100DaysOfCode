5af5391b19a0500a15d29be5be734610d75656d0
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2023-01-17 22:38:31 -0800

    Update CaesarCipher.py

--- day8_FunctionParameters/CaesarCipher/CaesarCipher.py
+++ day8_FunctionParameters/CaesarCipher/CaesarCipher.py
@@ -4,33 +4,52 @@
 # TODO-4: Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
 # TODO-5: Shift each letter of the 'text' *backwards* by the shift amount and print the decrypted text.
 # TODO-6: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable.
+# TODO-7: Combine encrypt() and decrypt() functions into a single function called caesar()
+# TODO-8: Call the caesar() function w/ 'text', 'shift' and 'direction' values
+# TODO-9: Import and print the logo from art.py when the program starts.
+# TODO-10: What if the user enters a shift that is greater than the number of letters in the alphabet?
+# TODO-11: What happens if the user enters a number/symbol/space?
+# TODO-12: Can you figure out a way to ask the user if they want to restart the cipher program?
+
+from art import logo
+print(logo)
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
             'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt: ")
-text = input("Type your message: ").lower()
-shift = int(input("Type the shift number: "))
 
-
-# TODO-7: Combine encrypt() and decrypt() functions into a single function called caesar()
 def caesar(start_text, shift_amount, cipher_direction):
     end_text = ""
     if cipher_direction == "decode":
         shift_amount *= -1
-    for letter in start_text:
-        position = alphabet.index(letter)
-        new_position = position + shift_amount
-        end_text += alphabet[new_position]
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
     print(f"The {cipher_direction} text is {end_text}")
 
 
-# TODO-8: Call the caesar() function w/ 'text', 'shift' and 'direction' values
-caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+should_continue = True
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt: ")
+    text = input("Type your message: ").lower()
+    shift = int(input("Type the shift number: "))
+    
+    shift = shift % 26
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+    result = str(input("Type 'yes' if you want to go again. Otherwise, type 'no': "))
+    if result == "no":
+        should_continue = False
+        print("Thanks! Goodbye.")
 
 
 # TODO: The code below has been condensed into a single function called caesar()
+
 # def encrypt(plain_text, shift_amount):
 #     cipher_text = ""
 #     for letter in plain_text:
