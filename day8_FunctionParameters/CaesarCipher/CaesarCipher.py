f58caf38150c60c4ea6d5c2a043fa7825cd1fd95
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2023-01-17 21:40:24 -0800

    Update CaesarCipher.py

--- day8_FunctionParameters/CaesarCipher/CaesarCipher.py
+++ day8_FunctionParameters/CaesarCipher/CaesarCipher.py
@@ -1,3 +1,10 @@
+# TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+# TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount.
+# TODO-3: Call the encrypt function & pass in the user inputs. You should be able to test the code & encrypt a message.
+# TODO-4: Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
+# TODO-5: Shift each letter of the 'text' *backwards* by the shift amount and print the decrypted text.
+# TODO-6: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable.
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
             'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
@@ -7,7 +14,6 @@ text = input("Type your message: ").lower()
 shift = int(input("Type the shift number: "))
 
 
-# TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount.
 def encrypt(plain_text, shift_amount):
     cipher_text = ""
     for letter in plain_text:
@@ -18,7 +24,16 @@ def encrypt(plain_text, shift_amount):
     print(f"The encoded text is {cipher_text}")
 
 
-# TODO-3: Call the encrypt function & pass in the user inputs. You should be able to test the code & encrypt a message.
-encrypt(plain_text=text, shift_amount=shift)
+def decrypt(cipher_text, shift_amount):
+    plain_text = ""
+    for letter in cipher_text:
+        position = alphabet.index(letter)
+        new_position = position - shift_amount
+        plain_text += alphabet[new_position]
+    print(f"The decoded text is {plain_text}")
 
 
+if direction == "encode":
+    encrypt(plain_text=text, shift_amount=shift)
+elif direction == "decode":
+    decrypt(cipher_text=text, shift_amount=shift)
