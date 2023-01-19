429a9b3fbdd4b6a06bec2c9afb302c02cf06b42a
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2023-01-18 16:34:10 -0800

    Update GradingProgram.py

--- day9_DictAndNesting/GradingProgram.py
+++ day9_DictAndNesting/GradingProgram.py
@@ -1,3 +1,9 @@
+# You have access to a database of student_scores in the format of a dictionary.
+# The keys in student_scores are the names of the students and the values are their exam scores.
+# Write a program that converts their scores to grades. 
+# Make a new dictionary called student_grades that should contain student names for keys and their grades for values. 
+# The final version of the student_grades dictionary will be checked.
+
 student_scores = {
     "Harry": 81,
     "Ron": 78,
@@ -9,7 +15,7 @@ student_scores = {
 # TODO-1: Create an empty dictionary called student_grades.
 student_grades = {}
 for student in student_scores:
-    score = student_scores[student]
+    score = (student_scores[student])
     if score > 90:
         student_grades[student] = "Outstanding"
     elif score > 80:
@@ -18,4 +24,5 @@ for student in student_scores:
         student_grades[student] = "Acceptable"
     else:
         student_grades[student] = "Fail"
+
 print(student_grades)
