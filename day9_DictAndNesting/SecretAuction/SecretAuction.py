2f0432ada699fea3c1cb8002d45a0af374b5801a
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2023-01-18 19:35:41 -0800

    Update SecretAuction.py

--- day9_DictAndNesting/SecretAuction/SecretAuction.py
+++ day9_DictAndNesting/SecretAuction/SecretAuction.py
@@ -1,15 +1,23 @@
 bids = {}
 bidding_finished = False
 
+
+def highest_bidder(biding_record):
+    highest_bid = 0
+    winner = ""
+    for bidder in biding_record:
+        bid_amount = biding_record[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print(f"The winner is {winner} with the highest bid of ${highest_bid}")
+
+
 while not bidding_finished:
     name = str(input("What is your name: "))
     price = int(input("What is your bid: $"))
     bids[name] = price
     should_continue = str(input("Bid again? Select 'yes' or 'no': ")).lower()
-
-if add_name == "no":
-    top_bid = {}
-    for bid in int(bid_dict[bid]):
-        max_bid = max(bid)
-    print(f"{[name]} made the highest bid of ${[max_bid]} dollars!")
-
+    if should_continue == "no":
+        bidding_finished = True
+        highest_bidder(bids)
