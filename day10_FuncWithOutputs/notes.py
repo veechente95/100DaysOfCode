e3cc19adfdbd617ba89e3d497aafc612333a10dc
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2023-01-19 13:29:37 -0800

    Create notes.py

---
+++ day10_FuncWithOutputs/notes.py
@@ -0,0 +1,10 @@
+# function with outputs
+def format_name(f_name, l_name):
+    formatted_first = f_name.title()
+    formatted_last = l_name.title()
+
+    return f"{formatted_first} {formatted_last}"
+
+
+format_name("ViCENte", "CEJA")
+
