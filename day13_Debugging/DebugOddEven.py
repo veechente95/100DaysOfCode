37863fad0a50044a1a47d2a68541ae681b36830c
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2023-01-26 13:49:32 -0800

    Update DebugOddEven.py

--- day13_Debugging/DebugOddEven.py
+++ day13_Debugging/DebugOddEven.py
@@ -1,7 +1,16 @@
-# Exercise 1 - Odd Even
-# added "==" after 2
+# Exercise 1 - Debugg Odd Or Even
 number = int(input("Which number do you want to check?"))
-if number % 2 == 0:
-    print("This is an even number.")
+
+if number % 2 = 0:
+  print("This is an even number.")
 else:
-    print("This is an odd number.")
+  print("This is an odd number.")
+
+# Correct Answer
+number = int(input("Which number do you want to check?"))
+
+if number % 2 == 0:   # '=' needs to read as '=='
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
+  print("This is an odd number.")
