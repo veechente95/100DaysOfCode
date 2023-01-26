cf95653b00aee56d7eb436414c6017517c7a3f7a
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2023-01-25 20:41:28 -0800

    Update debugg_exercises.py

--- day13_Debugging/debugg_exercises.py
+++ day13_Debugging/debugg_exercises.py
@@ -1,17 +1,22 @@
-# TODO: Debugging Exercises
+# TODO Debugging Exercises
 
+# TODO 1) Describe the problem
 # Range needs to be (1, 21) if i == 20. Computer counts from 0, 1, 2...
 def my_function():
-    for i in range(1, 20):
+    for i in range(1, 20):   # change to (1, 21)
         if i == 20:
             print("You got it!")
-      
-        
-# Correct way
-def my_function():
-    for i in range(1, 21):
-        if i == 20:
-            print("You got it!")
-
 
 my_function()
+
+
+# TODO 2)  Reproduce the bug
+# IndexError - dice images list counts from 0 to 5
+from random import randint
+dice_images = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣"]
+dice_num = randint(1, 6)    # change to (0, 5)
+print(dice_images[dice_num])
+
+
+# TODO 3)  Play Computer - Pretend you are reading code line by line
+year = int
