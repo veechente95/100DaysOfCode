d359e6bd0328825e9924a05ef7a2c43b13ca9dbc
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2023-01-26 17:33:56 -0800

    Update HigherLowerGame.py

--- day14_HigherLowerGame/HigherLowerGame.py
+++ day14_HigherLowerGame/HigherLowerGame.py
@@ -12,8 +12,8 @@ def format_data(account):
     """Pulls account name, description and country key data from GameData data. """
     name = account["name"]
     description = account["description"]
-    country = account["location"]
-    return f"{name}, {description}, {country}"
+    country = account["country"]
+    return f"{name}, a {description} from {country}."
 
 
 def check_answer(guess, a_followers, b_followers):
@@ -22,19 +22,36 @@ def check_answer(guess, a_followers, b_followers):
         return guess == "a"
     else:
         return guess == "b"
-    
+
 
 def play_game():
+    """This runs the game while True"""
     print(logo)
-    print(f"Compare A: {name}, a {description}, from {location}.")
-    print(vs)
-    print(f"Against B: {name}, a {description}, from {location}.")
+    score = 0
+    game_continue = True
+    account_a = get_random_account()
+    account_b = get_random_account()
 
-    print(str(input("Who has more followers? Type 'A' or 'B': ")))
+    while game_continue:
+        account_a = account_b
+        account_b = get_random_account()
 
+        print(f"Compare A: {format_data(account_a)}")
+        print(vs)
+        print(f"Against B: {format_data(account_b)}")
 
+        guess = (str(input("Who has more followers? Type 'a' or 'b': "))).lower()
+        a_follower_count = account_a["follower_count"]
+        b_follower_count = account_b["follower_count"]
+        is_correct = check_answer(guess, a_follower_count, b_follower_count)
 
-
+        print(logo)
+        if is_correct:
+            score += 1
+            print(f"You got it right! Current score: {score}.")
+        else:
+            game_continue = False
+            print(f"Sorry, you got it wrong. Final score: {score}.")
 
 
 play_game()
