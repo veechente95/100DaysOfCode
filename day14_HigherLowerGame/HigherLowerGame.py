5b5ebd285782783a1ea152682a164835d39db1d4
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2023-01-26 17:14:26 -0800

    Update HigherLowerGame.py

--- day14_HigherLowerGame/HigherLowerGame.py
+++ day14_HigherLowerGame/HigherLowerGame.py
@@ -2,68 +2,39 @@ import random
 from GameData import data
 from HigherLowerGameArt import logo, vs
 
-# Generate a random account from the game data.
+
 def get_random_account():
     """Get data from random account"""
     return random.choice(data)
 
 
-# Format account data into printable format.
 def format_data(account):
-    """Format account into printable format: name, description and country"""
+    """Pulls account name, description and country key data from GameData data. """
     name = account["name"]
     description = account["description"]
-    country = account["country"]
-    return f"{name}, a {description}, from {country}"
+    country = account["location"]
+    return f"{name}, {description}, {country}"
 
 
 def check_answer(guess, a_followers, b_followers):
-    """Checks followers against user's guess
-    and returns True if they got it right.
-    Or False if they got it wrong."""
+    """Compares user and computer guess and returns True if they got it right OR False if they got it wrong."""
     if a_followers > b_followers:
         return guess == "a"
     else:
         return guess == "b"
+    
 
-
-def game():
+def play_game():
     print(logo)
-    score = 0
-    game_should_continue = True
-    account_a = get_random_account()
-    account_b = get_random_account()
+    print(f"Compare A: {name}, a {description}, from {location}.")
+    print(vs)
+    print(f"Against B: {name}, a {description}, from {location}.")
 
-    # Make game repeatable.
-    # Make B become the next A.
-    while game_should_continue:
-        account_a = account_b
-        account_b = get_random_account()
-
-        while account_a == account_b:
-            account_b = get_random_account()
-
-        print(f"Compare A: {format_data(account_a)}.")
-        print(vs)
-        print(f"Against B: {format_data(account_b)}.")
-
-        # Ask user for a guess.
-        ## Get follower count.
-
-        guess = input("Who has more followers? Type 'A' or 'B': ").lower()
-        a_follower_count = account_a["follower_count"]
-        b_follower_count = account_b["follower_count"]
-        is_correct = check_answer(guess, a_follower_count, b_follower_count)
-
-        # Check if user is correct and give feedback
-        # Keep track of score
-        print(logo)
-        if is_correct:
-            score += 1
-            print(f"You're right! Current score: {score}.")
-        else:
-            game_should_continue = False
-            print(f"Sorry, that's wrong. Final score: {score}")
+    print(str(input("Who has more followers? Type 'A' or 'B': ")))
 
 
-game()
+
+
+
+
+play_game()
