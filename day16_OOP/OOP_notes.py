ddf3b674d7c03fc234e49f3f70156ae75bbab274
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2023-01-31 10:33:58 -0800

    Create OOP_notes.py

---
+++ day16_OOP/OOP_notes.py
@@ -0,0 +1,13 @@
+# OOP combines some data like attributes (variables) and functionality like methods (functions).
+
+class Waiter:
+    # Attributes or variables
+    is_holding_plate = True
+    tables_responsible = [4, 5, 6]
+
+    # Methods or functionality
+    def take_order(table, order):
+        # takes order to chef
+
+    def take_payment(amount):
+        # adds money to restaurant
