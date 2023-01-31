6c4134feb260214208a510325a713633dbf0277b
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2023-01-31 11:08:44 -0800

    Update OOP_notes.py

--- day16_OOP/OOP_notes.py
+++ day16_OOP/OOP_notes.py
@@ -1,5 +1,4 @@
 # OOP combines some data like attributes (variables) and functionality like methods (functions).
-
 class Waiter:
     # Attributes or variables
     is_holding_plate = True
@@ -11,3 +10,31 @@ class Waiter:
 
     def take_payment(amount):
         # adds money to restaurant
+        
+  
+# Create a class - Class are made using Pascal case (all new words start with an upper case letter)
+
+# car = object
+# Class = CarBlueprint
+car = CarBlueprint()
+
+
+# Importing Turtle class example 
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("aquamarine3")
+my_screen = Screen()
+timmy.forward(100)
+timmy.left(90)
+
+my_screen.exitonclick()
+
+
+# Importing PrettyTable class example
+from prettytable import PrettyTable
+table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+table.align = "l"
